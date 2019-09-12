540cbeaa6d0020bc28ef1f6c6daf56033cc830f2
Klaus Foerster (marvin) <klaus.foerster@mhcomm.fr>
2019-09-12 14:33:53 +0200

    module for creating interruptable sleeps
    sleeps that can be cancelled.

---
+++ pypeman/helpers/sleeper.py
@@ -0,0 +1,38 @@
+import asyncio
+
+class Sleeper:
+    """
+    Group sleep calls allowing instant cancellation of all
+
+    found at: https://stackoverflow.com/questions/37209864/interrupt-all-asyncio-sleep-currently-executing
+    """
+
+    def __init__(self, loop):
+        self.loop = loop
+        self.tasks = set()
+
+    async def sleep(self, delay, result=None):
+        coro = asyncio.sleep(delay, result=result, loop=self.loop)
+        task = asyncio.ensure_future(coro)
+        self.tasks.add(task)
+        try:
+            return await task
+        except asyncio.CancelledError:
+            return result
+        finally:
+            self.tasks.remove(task)
+
+    def cancel_all_helper(self):
+        "Cancel all pending sleep tasks"
+        cancelled = set()
+        for task in self.tasks:
+            if task.cancel():
+                cancelled.add(task)
+        return cancelled
+
+    async def cancel_all(self):
+        "Coroutine cancelling tasks"
+        cancelled = self.cancel_all_helper()
+        await asyncio.wait(self.tasks)
+        self.tasks -= cancelled
+        return len(cancelled)

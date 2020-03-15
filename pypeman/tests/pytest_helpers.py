9c8a49a49e60568cb41d543063ce1ea79f75614b
kf@hydra <kf@hydra>
2020-03-15 02:50:25 +0100

    add pytest helper

---
+++ pypeman/tests/pytest_helpers.py
@@ -0,0 +1,23 @@
+"""
+Helpers / fixtures for pytest
+"""
+
+import pytest
+
+import pypeman.channels
+import pypeman.endpoints
+import pypeman.nodes
+
+
+@pytest.fixture(scope="function")
+def clear_graph():
+    n_nodes = len(pypeman.nodes.all_nodes)
+    n_channels = len(pypeman.channels.all_channels)
+    n_endpoints = len(pypeman.endpoints.all_endpoints)
+    pypeman.nodes.reset_pypeman_nodes()
+    pypeman.channels.reset_pypeman_channels()
+    pypeman.endpoints.reset_pypeman_endpoints()
+    yield n_nodes, n_channels, n_endpoints
+    pypeman.nodes.reset_pypeman_nodes()
+    pypeman.channels.reset_pypeman_channels()
+    pypeman.endpoints.reset_pypeman_endpoints()

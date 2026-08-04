cd780ebb46517b54caa5f1176923432536fdc5b2
Grenier Célestin <43825779+naclsn@users.noreply.github.com>
2026-08-04 16:33:48 +0200

    Add db sqlite msgstore (#326)
    Co-authored-by: celestin-Thin-15-B12UCX <celestin.grenier@mhcomm.fr>

--- setup.py
+++ setup.py
@@ -65,6 +65,7 @@ setup(
         "python-dateutil",
         "websockets<14",  # loop problem in py 3.8+
         "aiohttp",
+        "aiosqlite",
         "jsonrpcclient",
         "jsonrpcserver<5",  # cannot import name 'Protocol' from 'typing' : fixed in py3.8
         "sqlitedict",
